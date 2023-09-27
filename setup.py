3d7350db14e3b8706a109fb44d922585ae60a21c
RifatM97 <ricioman.it@gmail.com>
2023-09-27 10:56:13 +0100

    requirments added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+# looks for __init__.py files and creates packages
+setup(
+    name = "wasteDetection",
+    version= '0.0.0',
+    author= 'Rifat Mahammod',
+    author_email= 'ricioman.it@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
